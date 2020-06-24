95e54b33817d398952ab5d9f76d840368667a933
JpabloMF <juanpablo45671@gmail.com>
2020-06-23 21:15:09 -0500

    getting damage from platforms

--- Scripts/GUI.gd
+++ Scripts/GUI.gd
@@ -4,17 +4,50 @@ onready var number_coins = $Container/CoinsContainer/Coins
 onready var number_lives = $Container/LivesContainer/Lives
 var coins_amount = 0
 var lives_amount = 3
+var _timer = null #checking every second if player is getting damage
+var danger_element_entered = false;
 
-signal printOk
+func get_timer():
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(1.5)
+	_timer.set_one_shot(false) # Make sure it loops
+	
+func start_timer():
+	_timer.start()
+	
+func stop_timer():
+	_timer.stop()
 
 func _ready():
 	number_coins.text = str(coins_amount)
 	number_lives.text = str(lives_amount)
+	get_timer()
 
+func reduce_lives():
+	lives_amount -= 1
+	number_lives.text = str(lives_amount)
+	
+func get_coin():
+	coins_amount += 1
+	number_coins.text = str(coins_amount)
+		
 func _on_Area2D_area_entered(area):
+	danger_element_entered = true
 	if "coin" in area.get_name():
-		coins_amount += 1
-		number_coins.text = str(coins_amount)
+		get_coin()
 	elif "DangerPlatform" in area.get_name():
-		lives_amount -= 1
-		number_lives.text = str(lives_amount)
+		start_timer()
+		if lives_amount-1 > 0:
+			reduce_lives()
+		else:
+			reduce_lives()
+
+func _on_Timer_timeout():
+	if danger_element_entered:
+		reduce_lives()
+
+func _on_Player_area_exited(area):
+	danger_element_entered = false
+	stop_timer()
--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -5,8 +5,25 @@ const ACCELERATION = 50
 const MAX_SPEED = 300
 var motion = Vector2()
 
+var _timer = null #checking every second if player is getting damage
+var danger_element_entered = false;
+
+func get_timer():
+	_timer = Timer.new()
+	add_child(_timer)
+	_timer.connect("timeout", self, "_on_Timer_timeout")
+	_timer.set_wait_time(1.5)
+	_timer.set_one_shot(false) # Make sure it loops
+	
+func start_timer():
+	_timer.start()
+	
+func stop_timer():
+	_timer.stop()
+
 func _ready():
-	pass # Replace with function body.
+	get_timer()
+
 
 func _physics_process(delta):
 	motion.y += 10
@@ -20,4 +37,23 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		if is_on_floor():
 			motion.y = -400
+			
+	if Input.is_action_pressed("ui_accept"):
+		get_tree().paused = false
 	motion = move_and_slide(motion,UP)
+
+
+func _on_Player_area_entered(area):
+	danger_element_entered = true
+	if "DangerPlatform" in area.get_name():
+		$AnimatedSprite.play("damage")
+		start_timer()
+
+func _on_Timer_timeout():
+	if danger_element_entered:
+		$AnimatedSprite.play("damage")
+
+func _on_Player_area_exited(area):
+	danger_element_entered = false
+	$AnimatedSprite.play("idle")
+	stop_timer()
