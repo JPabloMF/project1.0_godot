be8851d463222ae32350c700fdd9dae9531c2d78
JpabloMF <juanpablo45671@gmail.com>
2020-06-23 22:44:11 -0500

    climbing stairs

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,7 +6,9 @@ const MAX_SPEED = 300
 var motion = Vector2()
 
 var _timer = null #checking every second if player is getting damage
-var danger_element_entered = false;
+var danger_element_entered = false
+
+var stairs_entered = false
 
 func get_timer():
 	_timer = Timer.new()
@@ -24,9 +26,12 @@ func stop_timer():
 func _ready():
 	get_timer()
 
-
 func _physics_process(delta):
-	motion.y += 10
+	if stairs_entered:
+		motion.y = 0
+	else:
+		motion.y += 10
+		
 	if Input.is_action_pressed("ui_left"):
 		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
 	elif Input.is_action_pressed("ui_right"):
@@ -35,19 +40,23 @@ func _physics_process(delta):
 		motion.x = lerp(motion.x, 0, 0.2)
 		
 	if Input.is_action_pressed("ui_up"):
-		if is_on_floor():
+		if is_on_floor() or stairs_entered:
 			motion.y = -400
+	elif Input.is_action_pressed("ui_down"):
+		motion.y = +400
+		
 			
 	if Input.is_action_pressed("ui_accept"):
 		get_tree().paused = false
 	motion = move_and_slide(motion,UP)
-
 
 func _on_Player_area_entered(area):
 	danger_element_entered = true
 	if "DangerPlatform" in area.get_name():
 		$AnimatedSprite.play("damage")
 		start_timer()
+	elif "StairsArea" in area.get_name():
+		stairs_entered = true
 
 func _on_Timer_timeout():
 	if danger_element_entered:
@@ -57,3 +66,5 @@ func _on_Player_area_exited(area):
 	danger_element_entered = false
 	$AnimatedSprite.play("idle")
 	stop_timer()
+	if "StairsArea" in area.get_name():
+		stairs_entered = false
